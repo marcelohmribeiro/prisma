e3eb7222b7ec09dce1388e41268e3fcd8e3fed83
marcelohmribeiro <marcelomarquesribeiro1@gmail.com>
2026-03-31 19:48:41 -0300

    chore: adicionando novo campo em profile_platform_account

--- lib/projeto_prisma/accounts/profile_platform_account.ex
+++ lib/projeto_prisma/accounts/profile_platform_account.ex
@@ -8,6 +8,7 @@ defmodule ProjetoPrisma.Accounts.ProfilePlatformAccount do
   schema "profile_platform_accounts" do
     field :external_user_id, :string
     field :profile_url, :string
+    field :api_key, :string
 
     belongs_to :profile, Profile
     belongs_to :platform, Platform
@@ -15,8 +16,8 @@ defmodule ProjetoPrisma.Accounts.ProfilePlatformAccount do
 
   def changeset(account, attrs) do
     account
-    |> cast(attrs, [:external_user_id, :profile_url, :profile_id, :platform_id])
-    |> validate_required([:external_user_id, :profile_id, :platform_id])
+    |> cast(attrs, [:external_user_id, :profile_url, :profile_id, :platform_id, :api_key])
+    |> validate_required([:external_user_id, :profile_id, :platform_id, :api_key])
     |> unique_constraint([:profile_id, :platform_id])
   end
 end
